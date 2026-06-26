8320f6980e830af9e3aa9c5c8339b53d3e8b6144
Shadow132245 <shadow132245@github.com>
2026-06-26 22:37:57 +0300

    CI: use --ld-path to force clang to use MSYS2's ld.exe
    -fuse-ld=lld doesn't work because lld also has the 65535 export limit.
    The old MinGW ld has 'export ordinal too large' error.
    MSYS2's modern binutils ld handles the large export table, but clang
    can't find it due to conflicting MinGW installations. --ld-path tells
    clang exactly which linker binary to use, bypassing toolchain detection.

--- scripts/linker-x64.bat
+++ scripts/linker-x64.bat
@@ -2,4 +2,4 @@
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw64\lib\gcc\x86_64-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" -fuse-ld=lld --target=x86_64-w64-windows-gnu -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --ld-path=C:\msys64\mingw64\bin\ld.exe --target=x86_64-w64-windows-gnu -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
