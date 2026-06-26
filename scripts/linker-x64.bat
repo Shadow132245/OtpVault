1628475076e0abb590cd1ef152e2b8e8013a1ed4
Shadow132245 <shadow132245@github.com>
2026-06-26 18:44:21 +0300

    Fix linker: use -B and --gcc-toolchain to force MSYS2 ld over C:\mingw64 ld on CI

--- scripts/linker-x64.bat
+++ scripts/linker-x64.bat
@@ -2,4 +2,4 @@
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw64\lib\gcc\x86_64-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" --target=x86_64-w64-windows-gnu -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --target=x86_64-w64-windows-gnu -BC:\msys64\mingw64\bin --gcc-toolchain=C:\msys64\mingw64 -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
--- scripts/linker-x86.bat
+++ scripts/linker-x86.bat
@@ -2,4 +2,4 @@
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw32\lib\gcc\i686-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" --target=i686-w64-windows-gnu -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --target=i686-w64-windows-gnu -BC:\msys64\mingw64\bin --gcc-toolchain=C:\msys64\mingw64 -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
