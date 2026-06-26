7b9770d200a91c20fcd67a322dbccabb30440a3d
Shadow132245 <shadow132245@github.com>
2026-06-26 23:37:16 +0300

    CI: use triple-prefixed gcc name for MSYS2 linker
    MSYS2's mingw-w64-x86_64-gcc package installs GCC as
    x86_64-w64-mingw32-gcc.exe (not gcc.exe). Use the canonical name.

--- scripts/linker-x64.bat
+++ scripts/linker-x64.bat
@@ -2,4 +2,4 @@
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw64\lib\gcc\x86_64-w64-mingw32\*") do set "GCC_DIR=%%d"
-C:\msys64\mingw64\bin\gcc.exe -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
+C:\msys64\mingw64\bin\x86_64-w64-mingw32-gcc.exe -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
--- scripts/linker-x86.bat
+++ scripts/linker-x86.bat
@@ -2,4 +2,4 @@
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw32\bin;C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw32\lib\gcc\i686-w64-mingw32\*") do set "GCC_DIR=%%d"
-C:\msys64\mingw32\bin\gcc.exe -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
+C:\msys64\mingw32\bin\i686-w64-mingw32-gcc.exe -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
