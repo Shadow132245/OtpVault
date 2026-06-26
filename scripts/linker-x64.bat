119af660e313ca692e51f7dd1e55a73f6ed334f1
Shadow132245 <shadow132245@github.com>
2026-06-26 18:59:37 +0300

    Fix linker: use --ld-path to force MSYS2 ld instead of relying on GCC toolchain detection

--- scripts/linker-x64.bat
+++ scripts/linker-x64.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-set "PATH=C:\msys64\mingw64\bin;%PATH%"
+set "LD_PATH=C:\msys64\mingw64\bin\ld.exe"
 for /d %%d in ("C:\msys64\mingw64\lib\gcc\x86_64-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" --target=x86_64-w64-windows-gnu -BC:\msys64\mingw64\bin --gcc-toolchain=C:\msys64\mingw64 -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --target=x86_64-w64-windows-gnu --ld-path="%LD_PATH%" -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
