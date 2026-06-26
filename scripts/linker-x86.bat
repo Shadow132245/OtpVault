b51ce4ca5890de7ec8d8516df2ce988d36e255f8
Shadow132245 <shadow132245@github.com>
2026-06-26 19:34:27 +0300

    Neutralize old MinGW ld.exe on CI + linker script cleanup

--- scripts/linker-x86.bat
+++ scripts/linker-x86.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-set "LD_PATH=C:\msys64\mingw64\bin\ld.exe"
+set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw32\lib\gcc\i686-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" --target=i686-w64-windows-gnu --ld-path="%LD_PATH%" -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --target=i686-w64-windows-gnu -BC:\msys64\mingw64\bin -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
