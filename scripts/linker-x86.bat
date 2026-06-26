b085522c3da840ed1acbf7faad25355244f68944
Shadow132245 <shadow132245@github.com>
2026-06-26 23:42:20 +0300

    CI: set GCC_INSTALL_PREFIX env var + debug MSYS2 paths
    GCC_INSTALL_PREFIX tells clang exactly which MinGW installation root
    to use, overriding PATH-based detection. Add debug step to understand
    the actual MSYS2 installation layout on windows-latest.

--- scripts/linker-x86.bat
+++ scripts/linker-x86.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-set "PATH=C:\msys64\mingw32\bin;C:\msys64\mingw64\bin;%PATH%"
+set "PATH=C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw32\lib\gcc\i686-w64-mingw32\*") do set "GCC_DIR=%%d"
-C:\msys64\mingw32\bin\i686-w64-mingw32-gcc.exe -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
+"C:\Program Files\LLVM\bin\clang.exe" --target=i686-w64-windows-gnu -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
