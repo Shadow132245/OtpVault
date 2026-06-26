007c776354f28bae681b854553097aae4a8304a3
Shadow132245 <shadow132245@github.com>
2026-06-26 23:32:39 +0300

    CI: use MSYS2 gcc as linker instead of clang
    clang's --ld-path not supported on this version. Use gcc from MSYS2
    directly as the linker driver, which internally uses MSYS2's modern
    ld that supports large export tables (>65535 symbols).

--- scripts/linker-x86.bat
+++ scripts/linker-x86.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-set "PATH=C:\msys64\mingw64\bin;%PATH%"
+set "PATH=C:\msys64\mingw32\bin;C:\msys64\mingw64\bin;%PATH%"
 for /d %%d in ("C:\msys64\mingw32\lib\gcc\i686-w64-mingw32\*") do set "GCC_DIR=%%d"
-"C:\Program Files\LLVM\bin\clang.exe" --ld-path=C:\msys64\mingw32\bin\ld.exe --target=i686-w64-windows-gnu -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
+C:\msys64\mingw32\bin\gcc.exe -LC:\msys64\mingw32\lib -LC:\msys64\mingw32\lib\gcc\i686-w64-mingw32 -L"%GCC_DIR%" %*
