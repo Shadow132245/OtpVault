bc6d3ca1296daffeba578f7ab8b8849f91d7114a
Shadow132245 <shadow132245@github.com>
2026-06-26 20:12:30 +0300

    Revert to clang linker (original -B backslashes) + expanded neutralization (C:\mingw64 + C:\Strawberry ld/gcc/mingw32) to force MSYS2 ld

--- scripts/linker-x64.bat
+++ scripts/linker-x64.bat
@@ -1,4 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
 set "PATH=C:\msys64\mingw64\bin;%PATH%"
-x86_64-w64-mingw32-gcc %*
+for /d %%d in ("C:\msys64\mingw64\lib\gcc\x86_64-w64-mingw32\*") do set "GCC_DIR=%%d"
+"C:\Program Files\LLVM\bin\clang.exe" --target=x86_64-w64-windows-gnu -BC:\msys64\mingw64\bin -LC:\msys64\mingw64\x86_64-w64-mingw32\lib -LC:\msys64\mingw64\lib -L"%GCC_DIR%" %*
