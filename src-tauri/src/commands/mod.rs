e205930a2a3b548d86623d083fd8ac1ac65ca29d
Shadow132245 <shadow132245@users.noreply.github.com>
2026-08-19 01:04:38 +0300

    fix: use pub use instead of pub mod as for neon_http alias (Rust syntax)

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -5,4 +5,6 @@ pub mod email_auth;
 #[cfg(not(target_os = "android"))]
 pub mod neon;
 #[cfg(target_os = "android")]
-pub mod neon_http as neon;
+mod neon_http;
+#[cfg(target_os = "android")]
+pub use neon_http as neon;
